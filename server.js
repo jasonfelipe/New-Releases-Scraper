e4ccb87080f13676c5599dfdd7e4d10a5a01913c
Legend <jasonrfelipe@gmail.com>
2018-09-11 15:52:53 -0700

    added code for handlebars

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ var PORT = 3000;
 // Initialize Express
 var app = express();
 
-// Configure middleware
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
@@ -101,9 +104,9 @@ app.get("/scrape", function (req, res) {
                 }
                 console.log(movies);
                 db.Movie.create(movies)
-                .then(dbMovies => {
-                    console.log(dbMovies);
-                }).catch(err => res.json(err))
+                    .then(dbMovie => {
+                        console.log(dbMovie);
+                    }).catch(err => res.json(err))
             });
 
 
@@ -112,8 +115,41 @@ app.get("/scrape", function (req, res) {
     });
 });
 
+app.get("/movies/:id", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Movie.findOne({ _id: req.params.id })
+        // ..and populate all of the notes associated with it
+        .populate("note")
+        .then(dbMovie => {
+            // If we were able to successfully find a movie with the given id, send it back to the client
+            res.json(dbMovie);
+        })
+        .catch(err => {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
 
+// Route for saving/updating an Article's associated Note
+app.post("/movies/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+        .then(dbNote => {
+            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+            return db.Movie.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(dbMovie => {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbMovie);
+        })
+        .catch(err => {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
 //TO DO - 
 //GET NEW GAMES AND NEW ALBUMS INTO THERE (DB)
