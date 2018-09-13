a5c8ebd29d2e6c93892452dca25d115236e0865d
Legend <jasonrfelipe@gmail.com>
2018-09-13 14:25:58 -0700

    added mongo code for heroku

--- server.js
+++ server.js
@@ -32,8 +32,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newreleases");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 //===============================================
 //                  Routes
 //===============================================
